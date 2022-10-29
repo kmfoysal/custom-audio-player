af4ebebc544bc006cf16d4b143aa02347690b6d8
kmfoysal <faisal.cri@gmail.com>
2022-10-29 20:56:17 +0600

    add spped rate functionality

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -1,114 +1,113 @@
-import React, { useRef, useState } from 'react';
-import Song from '../../assets/audio/Kesariya.mp3';
+import React, { useEffect, useRef, useState } from "react";
+import Song from "../../assets/audio/Kesariya.mp3";
 import PauseIcon from "../../assets/icon/Pause.svg";
-import PlayIcon from '../../assets/icon/play.png';
-import './AudioPlayer.scss';
+import PlayIcon from "../../assets/icon/play.png";
+import "./AudioPlayer.scss";
 
 const AudioPlayer = () => {
+  const audioPlayerRef = useRef(null);
+  const progressTrackRef = useRef(null); // reference our progress bar
 
-    const audioPlayerRef = useRef(null);
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [mediaTime, setMediaTime] = useState(0);
-    const [duration, setDuration] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [mediaTime, setMediaTime] = useState(0);
+  const [duration, setDuration] = useState(0);
 
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const seconds = Math.floor(time % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${returnedMinutes}:${returnedSeconds}`;
+  };
 
-    function formatTime(time) {
-      let minutes = Math.floor(time / 60);
-      let timeForSeconds = time - minutes * 60; // seconds without counted minutes
-      let seconds = Math.floor(timeForSeconds);
-      let secondsReadable = seconds > 9 ? seconds : `0${seconds}`; // To change 2:2 into 2:02
-      return `${minutes}:${secondsReadable}`;
-    }
+  const togglePlaying = (event) => {
+    setIsPlaying(!isPlaying);
+    const audioPlayer = audioPlayerRef.current;
+    isPlaying ? audioPlayer.pause() : audioPlayer.play();
+  };
 
+  const onTimeUpdate = () => {
+    setMediaTime(Math.floor(audioPlayerRef.current.currentTime));
+  };
 
-    const togglePlaying = (event) => {
-      setIsPlaying(!isPlaying);
+  const onLoadedMetadata = () => {
+    setDuration(Math.floor(audioPlayerRef.current.duration));
+  };
 
-      const audioPlayer = audioPlayerRef.current;
-      isPlaying ? audioPlayer.pause() : audioPlayer.play();
-    };
+  const onTrackChange = (event) => {
+    const playhead = parseFloat(event.target.value);
+    setMediaTime(playhead);
+    audioPlayerRef.current.currentTime = playhead;
+  };
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const changeProgressTrackColor = () => {
+    progressTrackRef.current.style.setProperty(
+      "--track-before-width",
+      `${(progressTrackRef.current.value / duration) * 100}%`
+    );
+  };
 
-      const onLoadedMetadata = () => {
-        // let minutes = Math.floor(audioPlayerRef.current.duration / 60);
-        // let timeForSeconds = audioPlayerRef.current.duration - minutes * 60; // seconds without counted minutes
-        // let seconds = Math.floor(timeForSeconds);
-        // let secondsReadable = seconds > 9 ? seconds : `0${seconds}`; // To change 2:2 into 2:02
+  useEffect(() => {
+    changeProgressTrackColor();
+  }, [changeProgressTrackColor]);
 
-        setDuration((audioPlayerRef.current.duration));
-      };;
+  const onChangeSpeed = (newSpeed) => {
+    audioPlayerRef.current.playbackRate = newSpeed;
+  };
 
-      const onTimeUpdate = () => {
-
-        // let minutes = Math.floor(audioPlayerRef.current.currentTime / 60);
-        // let timeForSeconds = audioPlayerRef.current.currentTime - minutes * 60; // seconds without counted minutes
-        // let seconds = Math.floor(timeForSeconds);
-        // let secondsReadable = seconds > 9 ? seconds : `0${seconds}`; // To change 2:2 into 2:02
-    
-        setMediaTime((audioPlayerRef.current.currentTime));
-
-      };
-
-      const onTrackChange = (event) => {
-        const playhead = Math.floor(event.target.value);
-        setMediaTime(playhead);
-        audioPlayerRef.current.currentTime = playhead;
-      };
-
-      const onChangeSpeed = (newSpeed) => {
-        audioPlayerRef.current.playbackRate = newSpeed;
-      };
-
-    return (
-      <div>
-        <div className="custom-audio-player">
-          <div className="action-bar">
-            <div className="play">
-              {isPlaying ? (
-                <button className="play-pause" onClick={togglePlaying}>
-                  <img src={PauseIcon} alt={PauseIcon} />
-                  Click pause to stop listening
-                </button>
-              ) : (
-                <button className="play-pause" onClick={togglePlaying}>
-                  <img src={PlayIcon} alt={PlayIcon} />
-                  Click play to listen to the blog
-                </button>
-              )}
-            </div>
-            <div className="speed-rate">
-              <select>
-                <option value="1">1X</option>
-                <option value="2">1.5X</option>
-                <option value="2">2X</option>
-              </select>
-            </div>
+  return (
+    <div>
+      <div className="custom-audio-player">
+        <div className="action-bar">
+          <div className="play">
+            {isPlaying ? (
+              <button className="play-pause" onClick={togglePlaying}>
+                <img src={PauseIcon} alt={PauseIcon} />
+                Click pause to stop listening
+              </button>
+            ) : (
+              <button className="play-pause" onClick={togglePlaying}>
+                <img src={PlayIcon} alt={PlayIcon} />
+                Click play to listen to the blog
+              </button>
+            )}
           </div>
-          <div className="progress-track">
-            <p>{mediaTime}</p>
-            <input
-              type="range"
-              id="track"
-              value={mediaTime}
-              min={0}
-              max={duration}
-              onChange={onTrackChange}
-            //   aria-valuetext={`${mediaTime} seconds`}
-            />
-            <p>{duration}</p>
+          <div className="speed-rate">
+            <select onClick={(e) => onChangeSpeed(e.target.value)}>
+              {[1, 1.5, 2].map((speedOption, i) => (
+                <option key={i} value={speedOption}>
+                  {speedOption}X
+                </option>
+              ))}
+            </select>
           </div>
         </div>
-        <audio
-          ref={audioPlayerRef}
-          src={Song}
-          controls
-          onLoadedMetadata={onLoadedMetadata}
-          onTimeUpdate={onTimeUpdate}
-          onPlay={() => setIsPlaying(true)}
-          onPause={() => setIsPlaying(false)}
-        />
+        <div className="progress-track">
+          <p>{formatTime(mediaTime)}</p>
+          <input
+            type="range"
+            id="track"
+            value={mediaTime}
+            min={0}
+            max={duration}
+            ref={progressTrackRef}
+            onChange={onTrackChange}
+          />
+          <p>{duration && !isNaN(duration) && formatTime(duration)}</p>
+        </div>
       </div>
-    );
+      <audio
+        ref={audioPlayerRef}
+        src={Song}
+        // controls
+        onLoadedMetadata={onLoadedMetadata}
+        onTimeUpdate={onTimeUpdate}
+        onPlay={() => setIsPlaying(true)}
+        onPause={() => setIsPlaying(false)}
+      />
+    </div>
+  );
 };
 
 export default AudioPlayer;
